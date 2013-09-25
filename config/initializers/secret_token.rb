ab9f90b8122acf8d3e526edead9683c7bd538f9a
Conent <albertocontento@gmail.com>
2013-09-25 15:09:33 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaiserschutzen::Application.config.secret_token = '01b4e0ed521f28f24b5415b20fed30d2f06d7687fd4bfe6711cdc9d756fe4d50c4e3c238126a4d1fa525eec71f394babda07ae42e65922008d7c0cf9fc9ddea7'
