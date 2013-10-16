cb418b0fd498b28e0a12343b86c6f8ceda816dd6
Conent <albertocontento@gmail.com>
2013-10-16 20:10:50 +0100

    nuovo tentativo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,9 @@ Kaiserschutzen::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :s3_host_name = 's3-us-west-2.amazonaws.com'
     }
-    
+
     Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
   }
 end
