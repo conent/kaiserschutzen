3148f6f517346ed9b5c9571091c7c39a2d759975
Conent <albertocontento@gmail.com>
2013-10-16 01:17:02 +0100

    ulteriore error fix

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ActiveRecord::Base
   attr_accessible :story, :title, :image
-  has_attached_file :image, styles: { medium: "320x440>"}
+  has_attached_file :image, :styles => { :medium => "600x800>"}
 end
